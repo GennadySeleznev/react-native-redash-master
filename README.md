ffd524cdb2c12d449feb661be88ef4a90ae5b19a
gennadyseleznev <predradinkic@gmail.com>
2020-04-13 10:33:10 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# redash
+
+[![CircleCI](https://circleci.com/gh/wcandillon/react-native-redash.svg?style=svg)](https://circleci.com/gh/wcandillon/react-native-redash)
+[![npm version](https://badge.fury.io/js/react-native-redash.svg)](https://badge.fury.io/js/react-native-redash)
+
+Utility library for React Native Gesture Handler and Reanimated. As seen on the [“Can it be done in React Native?”](http://youtube.com/user/wcandill) YouTube series.
+
+## Installation
+
+```sh
+yarn add react-native-redash
+```
+
+## Documentation
+
+[https://wcandillon.github.io/react-native-redash](https://wcandillon.github.io/react-native-redash)
---
+++ packages/core/src/Vectors.ts
@@ -0,0 +1,60 @@
+import Animated from "react-native-reanimated";
+
+import { clamp as clamp1 } from "./Math";
+
+const { Value, block } = Animated;
+type Dimension = "x" | "y";
+type Fn = (...args: Animated.Adaptable<number>[]) => Animated.Node<number>;
+type BinArgOp<T extends Animated.Adaptable<number> | Vector = Vector> = [
+  T,
+  T,
+  ...T[]
+];
+
+export interface Vector<
+  T extends Animated.Adaptable<number> = Animated.Adaptable<number>
+> {
+  x: T;
+  y: T;
+}
+
+const create = <T extends Animated.Adaptable<number>>(x: T, y?: T) => ({
+  x,
+  y: y || x,
+});
+
+const createValue = (x: number, y: number) =>
+  create(new Value(x), new Value(y || x));
+
+const get = (vectors: Vector[], dimension: Dimension) =>
+  vectors.map((vector) => vector[dimension]);
+
+const apply = (fn: Fn, ...vectors: Vector[]) => ({
+  x: fn(...get(vectors, "x")),
+  y: fn(...get(vectors, "y")),
+});
+
+const add = (...vectors: BinArgOp) => apply(Animated.add, ...vectors);
+const sub = (...vectors: BinArgOp) => apply(Animated.sub, ...vectors);
+const multiply = (...vectors: BinArgOp) => apply(Animated.multiply, ...vectors);
+const divide = (...vectors: BinArgOp) => apply(Animated.divide, ...vectors);
+const clamp = (value: Vector, min: Vector, max: Vector) =>
+  apply(clamp1, value, min, max);
+
+const invert = (a: Vector) => multiply(create(-1), a);
+
+const set = (a: Vector<Animated.Value<number>>, b: Vector) =>
+  block([Animated.set(a.x, b.x), Animated.set(a.y, b.y)]);
+
+export const vec = {
+  create,
+  createValue,
+  invert,
+  add,
+  sub,
+  multiply,
+  divide,
+  set,
+  clamp,
+  apply,
+};
